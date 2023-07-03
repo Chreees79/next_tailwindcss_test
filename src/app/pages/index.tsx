e8bd47b97030267f322c8ddfcc2f6a81d725c237
Christelle Gévaert <christelle.gevaert32@gmail.com>
2023-07-03 18:41:58 +0200

    add style with flex to fix responsivness

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "./_Button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <p className="text-pink-300 hover:text-pink-600">Je suis un essai avec une API externe</p>
+      <h1 className="text-white hover:text-white/75 text-lg text-center">Je suis un essai avec une API externe</h1>
       <Button />
     </main>
   )
